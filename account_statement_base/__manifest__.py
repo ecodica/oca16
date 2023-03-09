7c70fc4c0893b347f4eec90d63e8ea854f0275e9
gkliska <goran@ecodica.eu>
2023-03-09 10:59:14 +0100

    OCA Update

--- account_statement_base/__manifest__.py
+++ account_statement_base/__manifest__.py
@@ -1,15 +1,16 @@
 # Copyright 2023 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Bank Statement Base",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.2.0",
     "category": "Accounting",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": "Base module for Bank Statements",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
+    "development_status": "Mature",
     "website": "https://github.com/OCA/account-reconcile",
     "depends": ["account"],
     "data": ["views/account_bank_statement_line.xml"],
