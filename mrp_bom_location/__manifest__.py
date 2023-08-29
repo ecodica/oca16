4de09d3547b06b236a4fef69493172c39168e8e8
gkliska <goran@ecodica.eu>
2023-08-29 12:49:02 +0200

    OCA Update

--- mrp_bom_location/__manifest__.py
+++ mrp_bom_location/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "MRP BOM Location",
     "summary": "Adds location field to Bill of Materials and its components.",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Manufacture",
     "website": "https://github.com/OCA/manufacture",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
@@ -11,5 +11,10 @@
     "application": False,
     "depends": ["mrp"],
     "data": ["views/mrp_view.xml", "views/report_mrpbomstructure.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "mrp_bom_location/static/src/xml/mrp_bom_overview_location.xml",
+        ],
+    },
     "installable": True,
 }
